6313c76682044345766d2f9f34cb80a359fa658a
matrzask <maciejtrzask@student.agh.edu.pl>
2025-10-24 08:46:02 +0200

    postgresql connection

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('../db');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows); // Send the query result as JSON
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Database query failed');
+  }
 });
 
 module.exports = router;
