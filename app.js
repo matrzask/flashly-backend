e225c8f3bd407157aaaa5bb821cd4120553c85cb
matrzask <maciejtrzask@student.agh.edu.pl>
2025-12-20 20:40:35 +0100

    added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var authRouter = require('./routes/auth');
 var decksRouter = require('./routes/decks');
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/auth', authRouter);
 app.use('/decks', decksRouter);
